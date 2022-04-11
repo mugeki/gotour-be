0835d6412d6d9bf633c61da7285c88d16c928719
Muhamad Elang Ramadhan <elangmugeki@gmail.com>
2022-04-11 13:27:33 +0700

    add logout route

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::get('/place', [PlaceController::class, 'get']);
 Route::get('/place/{id}', [PlaceController::class, 'get_by_id']);
